ff575c7ee2352e5ddfeeddbd910e650b42c2d72d
Simon Baslé <sbasle@vmware.com>
2021-01-15 12:31:31 +0100

    Handle pagination for IssueData query

--- src/main/java/io/spring/team/scorecard/graphql/GraphQLClient.java
+++ src/main/java/io/spring/team/scorecard/graphql/GraphQLClient.java
@@ -1,14 +1,16 @@
 package io.spring.team.scorecard.graphql;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Error;
+import com.apollographql.apollo.api.Input;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
 import io.spring.team.scorecard.AssignableUsersQuery;
@@ -25,6 +27,9 @@ import org.apache.commons.logging.LogFactory;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+import static reactor.core.publisher.Sinks.EmitFailureHandler.FAIL_FAST;
 
 public class GraphQLClient {
 
@@ -62,33 +67,62 @@ public class GraphQLClient {
 
 	public Flux<Issue> searchIssuesAndPRs(String searchQuery) {
 		logger.debug("query: " + searchQuery);
-		return Flux.create(sink -> {
-			this.apolloClient.query(new IssueDataQuery(searchQuery))
-					.enqueue(new ApolloCall.Callback<IssueDataQuery.Data>() {
-						@Override
-						public void onResponse(@NotNull Response<IssueDataQuery.Data> response) {
-							List<IssueDataQuery.Node> issues = response.getData().search().nodes();
+		Sinks.Many<Issue> sink = Sinks.many().unicast().onBackpressureBuffer();
+		AtomicInteger processed = new AtomicInteger(0);
 
-							logger.debug("processing " + issues.size() + " nodes, response indicated a total of " + response.getData().search().issueCount() + " matches");
+		ApolloCall.Callback<IssueDataQuery.Data> issueDataPaginatedCallback = new ApolloCall.Callback<IssueDataQuery.Data>() {
+			@Override
+			public void onResponse(@NotNull Response<IssueDataQuery.Data> response) {
+				if (response.hasErrors()) {
+					String messages = response.getErrors().stream().map(Error::getMessage).collect(Collectors.joining(", "));
+					sink.emitError(new IllegalStateException("Error in response: " + messages), FAIL_FAST);
+					return;
+				}
 
-							issues.stream()
-									.peek(node -> {
-										if (!(node instanceof IssueDataQuery.AsIssue) && !(node instanceof IssueDataQuery.AsPullRequest)) {
-											logger.debug("Excluding element that is not an issue: " + node.getClass().getName() + ": " + node);
-										}
-									})
-									.filter(node -> node instanceof IssueDataQuery.AsIssue || node instanceof IssueDataQuery.AsPullRequest)
-									.map(issue -> issueFromNode(issue))
-									.forEach(sink::next);
-							sink.complete();
-						}
+				final IssueDataQuery.Search search = response.getData().search();
 
-						@Override
-						public void onFailure(@NotNull ApolloException e) {
-							sink.error(e);
-						}
-					});
-		});
+				List<IssueDataQuery.Node> issues = search.nodes();
+				int issueCount = search.issueCount();
+				int start = processed.getAndAdd(issues.size());
+				int end = processed.get();
+				logger.info("Fetching issues " + start + ".." + end + "/" + issueCount);
+
+				emitIssues(issues, sink);
+
+				boolean hasMorePages = search.pageInfo().hasNextPage();
+				if (hasMorePages) {
+					logger.debug("Fetching additional page");
+					String nextCursor = search.pageInfo().endCursor();
+					apolloClient.query(new IssueDataQuery(searchQuery, Input.fromNullable(nextCursor)))
+					.enqueue(this);
+				}
+				else {
+					logger.debug("Last page");
+					sink.emitComplete(FAIL_FAST);
+				}
+			}
+
+			@Override
+			public void onFailure(@NotNull ApolloException e) {
+				sink.emitError(e, FAIL_FAST);
+			}
+		};
+		this.apolloClient.query(new IssueDataQuery(searchQuery, Input.absent()))
+					.enqueue(issueDataPaginatedCallback);
+
+		return sink.asFlux();
+	}
+
+	private void emitIssues(List<IssueDataQuery.Node> issues, Sinks.Many<Issue> sink) {
+		issues.stream()
+				.peek(node -> {
+					if (!(node instanceof IssueDataQuery.AsIssue) && !(node instanceof IssueDataQuery.AsPullRequest)) {
+						logger.debug("Excluding element that is not an issue: " + node.getClass().getName() + ": " + node);
+					}
+				})
+				.filter(node -> node instanceof IssueDataQuery.AsIssue || node instanceof IssueDataQuery.AsPullRequest)
+				.map(issue -> issueFromNode(issue))
+				.forEach(i -> sink.emitNext(i, FAIL_FAST));
 	}
 
 	@NotNull
